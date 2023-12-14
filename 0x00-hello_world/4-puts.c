e90f5bdfc8c1322780ab6f82fa175f7ec46323bc
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2023-12-13 23:11:31 -0800

    Write a C program that prints a sentence  followed by a new line.

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: Always 0 (success)
+ */
+
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return 0;
+}
+
