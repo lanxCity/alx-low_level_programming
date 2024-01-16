fb83904626c3e3a2dff34525b1e2b83994559bf3
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-01-16 01:41:45 -0800

    Create the static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Determines the length of a string
+ * @str: param
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+	/**/
+	for (i = 0; str[i]; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
