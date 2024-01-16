8a49481b2e534fa6e3a3704e45c11f76b1e60ac0
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-01-16 01:59:27 -0800

    updating

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -4,9 +4,10 @@
  * main - Entry point
  *
  * Return: success
- */ 
+ */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be \
+			the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
