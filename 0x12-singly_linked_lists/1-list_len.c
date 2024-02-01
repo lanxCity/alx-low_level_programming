c4ab9115c4c06c89bee63ea1585b816f371a30a5
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-02-01 15:42:01 -0800

    Updating project 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,11 +5,11 @@
  * @h: A pointer
  * Return: Returns the number of nodes
  */
- size_t list_len(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	size_t n;
 	list_t *nxt_add;
-	
+	/**/
 	nxt_add = (list_t *)h;
 
 	for (n = 0; nxt_add != NULL; n++)
