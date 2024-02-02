b4030674e522ec31448a3f4d6807c16a1aa70dcb
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-02-01 17:36:46 -0800

    Updating project 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,13 +1,16 @@
 #include "lists.h"
+
 /**
  * free_list - A function
  * @head: param1
  */
 void free_list(list_t *head)
 {
+	list_t *temp;
+	/**/
 	while (head != NULL)
 	{
-		list_t *temp = head;
+		temp = head;
 		head = head->next;
 		free(temp->str);
 		free(temp);
