a8554aa32c013137c455dfee00b889965abc5e5f
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-02-02 01:17:06 +0000

    Project 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_node - A function
+ * @head: param1
+ * @str: param2
+ * Return: Address of a node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node;
+	size_t i;
+	/**/
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+	/**/
+	new_node->str = strdup(str);
+	/**/
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	/**/
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
+	/**/
+	new_node->len = i;
+	new_node->next = NULL;
+	
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		list_t *current = *head;
+		
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		/**/
+		current->next = new_node;
+	}
+	return (new_node);
+}
