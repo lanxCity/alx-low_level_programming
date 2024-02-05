57736155ea6c5f4520c6574817993160deaa6555
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-02-05 09:52:25 +0000

    Add project 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - function
+ * @h: A pointer
+ * Return: Returns the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n;
+	listint_t *nxt_add;
+	/**/
+	nxt_add = (listint_t *)h;
+
+	for (n = 0; nxt_add != NULL; n++)
+	{
+		nxt_add = nxt_add->next;
+	}
+
+	return (n);
+}
