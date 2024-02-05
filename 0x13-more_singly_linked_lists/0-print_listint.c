e7b8824bee1fe308cb61fc990131717d21ab8fbc
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-02-05 09:34:32 +0000

    Add project 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - function
+ * @h: A pointer
+ * Return: Returns the number of nodes
+ */
+ size_t print_listint(const listint_t *h)
+{
+	size_t i;
+	listint_t *nxt_address;
+	
+	nxt_address = (list_t *)h;
+
+	for (i = 0; nxt_address != NULL; i++)
+	{
+		printf("[%d]\n", nxt_address->n);
+
+		nxt_address = nxt_address->next;
+	}
+
+	return (i);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -5,26 +5,29 @@
 #include <stddef.h>
 #include <stdlib.h>
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct listint_s - singly linked list
+ * @n: integer
  * @next: points to the next node
  * Description: singly linked list node structure
  */
-typedef struct list_s
+typedef struct listint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
 
 /*Prototypes*/
 int _putchar(char c);
-size_t print_list(const list_t *h);
-int _strlen(const char *s);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
+size_t print_listint(const listint_t *h);
+
+
+
+
+
+
+
+
+
+
 #endif
