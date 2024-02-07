d37d4f9d924e3149ca6f42494f5f56fddd136b90
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-02-07 12:46:36 -0800

    Project 9: Bettty issue

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 
 /**
- * pop_listint - A function
- * @head: param1
+ * insert_nodeint_at_index - A function
+ * @head: param
+ * @idx: param1
+ * @n: param2
  * Return: int
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-
 	listint_t *curr_node, *new_node;
 	unsigned int i;
 
@@ -15,7 +16,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
@@ -25,23 +25,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		 new_node->next = *head;
 		 *head = new_node;
+		
 		 return (new_node);
 	}
 
 	curr_node = *head;
-
 	for (i = 1; i < idx && curr_node != NULL; i++)
-	{
-
 		curr_node = curr_node->next;
-	}
-
 	if (curr_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	new_node->next = curr_node->next;
 	curr_node->next = new_node;
 
