d3425ef24e6b68a9dc8bfedc6d3876c7e5f0c23f
Abdulramon Sodiq <sodiqramon0gmail.com>
2024-02-13 11:54:58 +0000

    Add project 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - A function
+ * @filename: param1
+ * @letters: param2
+ * Return: the number of letters
+ */
+int create_file(const char *filename, char *text_content)
+{
+	size_t n;
+	int fadd;
+	char c;
+	/*ssize_t bytes;*/
+
+	if (filename == NULL)
+		return (-1);
+
+	fadd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
+	if (fadd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (n = 0; text_content[n] != '\0'; n++)
+		{
+			c = text_content[n];
+			
+			if (write(fadd, &c, 1) != 1)
+			{
+				close(fadd);
+				return (-1);
+			}
+		}
+	}
+
+	close(fadd);
+	return (1);
+}
