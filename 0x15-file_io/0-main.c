0573baada8384bc0c24dcf746bc590077bef571e
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-02-13 01:32:39 -0800

    Updating project 0

--- 0x15-file_io/0-main.c
+++ 0x15-file_io/0-main.c
@@ -4,12 +4,14 @@
 
 /**
  * main - check the code
+ * @ac: param1
+ * @av: param2
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
 	ssize_t n;
-	
+	/**/
 	if (ac != 2)
 	{
 		dprintf(2, "Usage: %s filename\n", av[0]);
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		if (write(1, &c, 1) != 1)
 			return (0);
-	}	
+	}
 
 	fclose(fadd);
 
