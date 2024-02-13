b504acbbebeffcc9059249ee5c6e471facb413ab
Abdulramon Sodiq <Sodiqramon0@gmail.com>
2024-02-13 06:31:00 -0800

    Corrected project 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fadd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	fadd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR);
 
 	if (fadd == -1)
 		return (-1);
@@ -36,5 +36,9 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	close(fadd);
+	
+	if (chmod(filename, S_IRUSR | S_IWUSR) == -1)
+		return (-1);
+
 	return (1);
 }
